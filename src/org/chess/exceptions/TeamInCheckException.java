ee4bcb5636b84f2c88d0498bd42a6f6719f9887c
tulioober019 <134515143+tulioober019@users.noreply.github.com>
2025-01-06 13:49:18 +0100

    Commit

--- src/org/chess/exceptions/TeamInCheckException.java
+++ src/org/chess/exceptions/TeamInCheckException.java
@@ -7,4 +7,7 @@ public class TeamInCheckException extends Exception {
 	 */
 	private static final long serialVersionUID = 1L;
 
+	public TeamInCheckException(Throwable cause) {
+		super("Your team is already in check. Only the queen is movable", cause);
+	}
 }
